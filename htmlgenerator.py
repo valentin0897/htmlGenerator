24f3feced361e9a32d3cd68c7cbe12cefcc125f2
= <valentin08@mail.ru>
2018-02-22 02:47:13 +0700

    ready

--- htmlgenerator.py
+++ htmlgenerator.py
@@ -1,9 +1,23 @@
 import numpy as np
 from functools import reduce
 
-matrix = np.array([[0,1,1,0], [0,0,0,0], [0,1,0,1], [0,0,1,0]], bool)
+path_index = 'index.html'
+path_matrix = 'matrix.txt'
+path_new_file = 'generated'
+mark = "links"
+matrix = []
+# matrix = np.array([[0,1,1,0], [0,0,0,0], [0,1,0,1], [0,0,1,0]], bool)
+with open(path_matrix, 'r', encoding='utf-8-sig') as file:
+    for line in file.readlines():
+        matrix.append(list(line.split()))
 
-# matrix = np.dot(matrix, matrix)
+for i, row in enumerate(matrix):
+    matrix[i] = list(map(int, row))
+
+matrix = np.array(matrix, bool)
+
+with open(path_index, 'r', encoding='utf-8') as file:
+    html = file.read()
 
 def get_adjacency_matrix(matrix):
     powered_list = [matrix]
@@ -12,4 +26,28 @@ def get_adjacency_matrix(matrix):
     result = reduce(lambda x, y: x + y, powered_list)
     return result
 
-print(get_adjacency_matrix(matrix))
+def get_links(matrix):
+    counter_i = 0
+    for i in matrix:
+        links = []
+        counter_j = 0
+        for j in i:
+            if(j):
+                links.append(counter_j)
+            counter_j += 1
+        write_file(counter_i, links)
+        counter_i += 1
+        
+def write_file(i, links):
+    with open(path_new_file + "\\" + str(i) +'.html', 'w', encoding='utf-8') as file:
+        for line in html.splitlines(True):
+            if mark in line:
+                file.write(line)
+                for link in links:
+                    file.write("<a href=\"" + str(link) + ".html\">Ссылка " + str(link) + "</a>\n")
+            else:
+                file.write(line)
+                            
+matrix = get_adjacency_matrix(matrix)
+print(matrix)
+get_links(matrix)
